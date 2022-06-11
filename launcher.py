ef83c6c51e6c4b26a39b034ee0b9c4509d355e10
RemiPoletoukhine <poletukhin.rs@phystech.edu>
2022-06-11 23:39:07 +0300

    Minor fixes

--- launcher.py
+++ launcher.py
@@ -3,42 +3,99 @@ import pandas as pd
 import numpy as np
 
 
-# process of aggregated analysis, i.e. analysis of a series of launches
+# process of aggregated analysis, i.e. analysis of series of launches
+
+# repeating for several probs and c_interval
+def several_intervals(intervals, probabilities):
+    # yes, another df
+    data_out = pd.DataFrame(
+        data=[],
+        columns=[
+            "Customer",
+            "NominalPossibility",
+            "CInterval",
+            "SeatsRequired",
+            "ArrivalTime",
+            "PreBooking",
+            "QueueingStart",
+            "QueueingFinish",
+            "PaymentStart",
+            "PaymentFinish",
+            "BookingStart",
+            "BookingFinish",
+            "Table",
+            "EatingStart",
+            "EatingFinish",
+        ],
+    )
+    seed_val = np.random.randint(1, 12345678)
+    for interval in intervals:
+        for probability in probabilities:
+            data_out = pd.concat(
+                [data_out, main.launcher(probability, seed_val, interval)],
+                ignore_index=True,
+            )
+
+    return data_out
 
 
-# repeating the process of getting the data for each probability in list
+# repeating the process of getting the data
+# for each probability in list
 def several_probs(probs):
     # and yet another DataFrame for overall data
-    inter_data = pd.DataFrame(data=[], columns=['Customer', 'Possibility', 'PreBooking',
-                                                'QueueingTime', 'PaymentTime', 'BookingTime',
-                                                'EatingTime', 'TotalTime'])
+    inter_data = pd.DataFrame(
+        data=[],
+        columns=[
+            "Customer",
+            "Possibility",
+            "PreBooking",
+            "QueueingTime",
+            "PaymentTime",
+            "BookingTime",
+            "EatingTime",
+            "TotalTime",
+        ],
+    )
 
-    # repeating for each possibility: 0.1, 0.2, ..., 1 with identical random values each times
+    # repeating for each possibility: 0.1, 0.2, ..., 1
+    # with identical random values each times
     seed_val = np.random.randint(1, 12345678)
     for possibility in probs:
         # firstly, create a DataFrame for each current data
-        aggr_data = pd.DataFrame(data=[], columns=['Customer', 'Possibility', 'PreBooking',
-                                                   'QueueingTime', 'PaymentTime', 'BookingTime',
-                                                   'EatingTime', 'TotalTime'])
+        aggr_data = pd.DataFrame(
+            data=[],
+            columns=[
+                "Customer",
+                "Possibility",
+                "PreBooking",
+                "QueueingTime",
+                "PaymentTime",
+                "BookingTime",
+                "EatingTime",
+                "TotalTime",
+            ],
+        )
         # get data from the process
         data = main.launcher(possibility, seed_value=seed_val)
         # and sort it
-        data.sort_values(by=['NominalPossibility', 'Customer'], inplace=True, ignore_index=True)
+        data.sort_values(
+            by=["NominalPossibility", "Customer"], inplace=True, ignore_index=True
+        )
         # calculate the values
-        queueing_time = data['QueueingFinish'].subtract(data['QueueingStart'])
-        payment_time = data['PaymentFinish'].subtract(data['PaymentStart'])
-        booking_time = data['BookingFinish'].subtract(data['BookingStart'])
-        eating_time = data['EatingFinish'].subtract(data['EatingStart'])
-        total_time = data['EatingFinish'].subtract(data['ArrivalTime'])
+        queueing_time = data["QueueingFinish"].subtract(data["QueueingStart"])
+        payment_time = data["PaymentFinish"].subtract(data["PaymentStart"])
+        booking_time = data["BookingFinish"].subtract(data["BookingStart"])
+        eating_time = data["EatingFinish"].subtract(data["EatingStart"])
+        total_time = data["EatingFinish"].subtract(data["ArrivalTime"])
         # and fill them
-        aggr_data['Customer'] = data['Customer']
-        aggr_data['Possibility'] = data['NominalPossibility']  # .astype(float)
-        aggr_data['PreBooking'] = data['PreBooking']
-        aggr_data['QueueingTime'] = queueing_time
-        aggr_data['PaymentTime'] = payment_time
-        aggr_data['BookingTime'] = booking_time
-        aggr_data['EatingTime'] = eating_time
-        aggr_data['TotalTime'] = total_time
+        aggr_data["Customer"] = data["Customer"]
+        aggr_data["Possibility"] = data["NominalPossibility"]
+        aggr_data["PreBooking"] = data["PreBooking"]
+        aggr_data["QueueingTime"] = queueing_time
+        aggr_data["PaymentTime"] = payment_time
+        aggr_data["BookingTime"] = booking_time
+        aggr_data["EatingTime"] = eating_time
+        aggr_data["TotalTime"] = total_time
         # concatenate data for each possibility together
         inter_data = pd.concat([inter_data, aggr_data], ignore_index=True)
 
@@ -48,9 +105,19 @@ def several_probs(probs):
 # repeating the function above n times
 def several_times_probs(probs, n):
     # another df
-    final_data = pd.DataFrame(data=[], columns=['Customer', 'Possibility', 'PreBooking',
-                                                'QueueingTime', 'PaymentTime', 'BookingTime',
-                                                'EatingTime', 'TotalTime'])
+    final_data = pd.DataFrame(
+        data=[],
+        columns=[
+            "Customer",
+            "Possibility",
+            "PreBooking",
+            "QueueingTime",
+            "PaymentTime",
+            "BookingTime",
+            "EatingTime",
+            "TotalTime",
+        ],
+    )
     for _ in range(n):
         # concatenate all data together
         final_data = pd.concat([final_data, several_probs(probs)], ignore_index=True)
@@ -59,4 +126,6 @@ def several_times_probs(probs, n):
 
 
 # export the data
-several_times_probs(np.arange(0.0, 1.05, 0.05), 10).to_csv('analysis5_7.csv', index=False)
+several_intervals(
+    intervals=np.arange(0.0, 1.01, 0.01), probabilities=np.arange(0.0, 1.01, 0.01)
+).to_csv("tables_9.csv", index=False)
--- main.py
+++ main.py
@@ -19,9 +19,7 @@ C_AMOUNT = 100
 # client occurrences rate for exponential distribution (beta = 1 / lambda)
 BETA = 0.5
 # list of tables in café: 6 tables for two, 4 tables for three, 5 tables for four
-TABLE_LIST = [4, 4, 4, 4, 2, 2, 3, 2,
-              4, 3, 2, 4, 2,
-              3, 2, 4, 3, 4]
+TABLE_LIST = [4, 4, 3, 2, 2]
 # list to choose a seats_required value for customers from
 SEATS_LIST = [1, 2, 3, 4]
 # booking parameters for normal distribution
@@ -33,133 +31,210 @@ EATING_TIME_VAR = 1
 
 
 class Cafeteria:
-
     def __init__(self, env, food_num, pay_num, table_list, seed):
         self.env = env
         # seed
         self.seed = seed
-        # resource 1: serviceperson who forms the order
+        # resource 1: service staff who forms the order
         self.food = sp.Resource(env, food_num)
         # resource 2: checkout where customers pay for the order
         self.pay = sp.Resource(env, pay_num)
         # resource 3: list of tables in the cafeteria; each table is a resource
-        self.table_res_list = sp.FilterStore(env, capacity=len(table_list))
-        Table = namedtuple('Table', 'id, seats')
-        table_list = list(zip(list(range(len(table_list))), table_list))
+        self.table_res_list = sp.FilterStore(env,
+                                             capacity=len(table_list))
+        Table = namedtuple("Table", "id, seats")
+        table_list = list(zip(list(range(len(table_list))),
+                              table_list))
         for element in table_list:
             self.table_res_list.items.append(Table(element[0], element[1]))
 
     def order(self, client):
         # the process of ordering
-        service_time_random = stats.erlang.rvs(a=2, loc=FOOD_TIME_MEAN,
-                                               scale=FOOD_TIME_VAR, random_state=self.seed + client)
+        service_time_random = stats.erlang.rvs(
+            a=2,
+            loc=FOOD_TIME_MEAN,
+            scale=FOOD_TIME_VAR,
+            random_state=self.seed + client,
+        )
         yield self.env.timeout(service_time_random)
-        # print("Serviceman Mark composed the %s's order in %.2f minutes." % (client, service_time_random))
+        # print("Serviceman Mark composed the customer %s's
+        # order in %.2f minutes." % (client,
+        # service_time_random))
 
     def payment(self, client):
         # the process of payment
-        payment_time_random = stats.erlang.rvs(a=2, loc=PAY_TIME_MEAN,
-                                               scale=PAY_TIME_VAR, random_state=self.seed + client)
+        payment_time_random = stats.erlang.rvs(
+            a=2, loc=PAY_TIME_MEAN, scale=PAY_TIME_VAR, random_state=self.seed + client
+        )
         yield self.env.timeout(payment_time_random)
-        # print("Cashier Zuck finished the checkout of %s's order in %.2f minutes." % (client, payment_time_random))
+        # print("Cashier Zuck finished the checkout of customer %s's
+        # order in %.2f minutes." % (client,
+        # payment_time_random))
 
 
-def customer(env, name, seats_required, cafe, pre_booking, probability, seed, interval_after_me):
+def customer(
+    env, name, seats_required, cafe, pre_booking, probability, seed, interval_after_me
+):
     global df_stats
-    # the customer process (identified by "name") enters the cafeteria ("cafe")
+    # the customer process (identified by "name")
+    # enters the cafeteria ("cafe")
     arrival_time = env.now
-    # print("%s enters the cafeteria at %.2f." % (name, env.now))
+    # print("Customer %s enters the cafeteria at %.2f." % (name, env.now))
     has_vacant_tables = False
     for i in range(len(cafe.table_res_list.items)):
-        # print(cafe.table_res_list.items[i])
         if cafe.table_res_list.items[i].seats >= seats_required:
             has_vacant_tables = True
     # the process of table booking
     # the customer books a table in advance with a certain possibility
     nominal_possibility = round(probability, 2)
     if Pr.Prob(nominal_possibility):
-        # the option with pre-booking works only if no one seeks for the place (queue is empty)
+        # the option with pre-booking works only
+        # if no one seeks for the place (queue is empty)
         if has_vacant_tables:
-            # print("hmm, looks like I, %s, can book a table here" % name)
+            # print("hmm, looks like I, customer %s,
+            # can book a table here" % name)
             booking_start = env.now
-            table = yield cafe.table_res_list.get(lambda place: place.seats >= seats_required)
-            booking_time_random = abs(stats.norm.rvs(loc=BOOKING_TIME_MEAN,
-                                                     scale=BOOKING_TIME_VAR,
-                                                     random_state=seed + name))
+            table = yield cafe.table_res_list.get(
+                lambda place: place.seats >= seats_required
+            )
+            booking_time_random = abs(
+                stats.norm.rvs(
+                    loc=BOOKING_TIME_MEAN,
+                    scale=BOOKING_TIME_VAR,
+                    random_state=seed + name,
+                )
+            )
             yield cafe.env.timeout(booking_time_random)
-            # print("%s has been booking a table for %.2f." % (name, booking_time_random))
-            # print("%s booked a table at %.2f." % (name, env.now))
+            # print("Customer %s has been booking
+            # a table for %.2f." % (name, booking_time_random))
+            # print("Customer %s booked a table at %.2f." % (name, env.now))
             booking_finish = env.now
             pre_booking = 1
         else:
             pass
-            # print("looks like no vacant places for me, %s" % name)
+            # print("Looks like there is no vacant places for me, customer %s" % name)
 
     # the customer requests an order
     queueing_start = env.now
     with cafe.food.request() as request_food:
         yield request_food
-        # print("%s starts forming the order at %.2f." % (name, env.now))
+        # print("Customer %s starts forming the order at %.2f." % (name, env.now))
         yield env.process(cafe.order(name))
-        # print("%s proceeds to the checkout at %.2f." % (name, env.now))
+        # print("Customer %s proceeds to the checkout at %.2f." % (name, env.now))
     queueing_finish = env.now
 
     # and requests completion of the payment procedure
     payment_start = env.now
     with cafe.pay.request() as request_pay:
         yield request_pay
-        # print("%s starts the payment procedure at %.2f." % (name, env.now))
+        # print("Customer %s starts the payment procedure at %.2f." % (name, env.now))
         yield env.process(cafe.payment(name))
-        # print("%s finishes the payment procedure at %.2f." % (name, env.now))
+        # print("Customer %s finishes the payment procedure at %.2f." % (name, env.now))
     payment_finish = env.now
 
     # check if the customer has a booked table
     if not pre_booking:
         # customer looks for a table because hasn't booked in advance
-        # print("%s has no choice but to try to book a table for %d at %.2f." % (name, seats_required, env.now))
+        # print("Customer %s has no choice
+        # but to try to book a table for %d at %.2f." % (name,
+        # seats_required, env.now))
         booking_start = env.now
-        table = yield cafe.table_res_list.get(lambda place: place.seats >= seats_required)
-        booking_time_random = abs(stats.norm.rvs(loc=BOOKING_TIME_MEAN,
-                                                 scale=BOOKING_TIME_VAR,
-                                                 random_state=seed+name))
+        table = yield cafe.table_res_list.get(
+            lambda place: place.seats >= seats_required
+        )
+        booking_time_random = abs(
+            stats.norm.rvs(
+                loc=BOOKING_TIME_MEAN, scale=BOOKING_TIME_VAR, random_state=seed + name
+            )
+        )
         yield cafe.env.timeout(booking_time_random)
-        # print("%s booked a table at %.2f." % (name, env.now))
+        # print("Customer %s booked a table at %.2f." % (name, env.now))
         booking_finish = env.now
 
     # client eats
-    # print("%s starts eating at %.2f." % (name, env.now))
+    # print("Customer %s starts eating at %.2f." % (name, env.now))
     # eating requires no less than 5 minutes
     eating_start = env.now
-    eating_random = abs(stats.norm.rvs(loc=EATING_TIME_MEAN + seats_required,
-                                       scale=EATING_TIME_VAR,
-                                       random_state=seed+name))
+    eating_random = abs(
+        stats.norm.rvs(
+            loc=EATING_TIME_MEAN + seats_required,
+            scale=EATING_TIME_VAR,
+            random_state=seed + name,
+        )
+    )
     yield env.timeout(eating_random)
     eating_finish = env.now
-    # print("%s eats for %.2f." % (name, eating_random))
+    # print("Customer %s eats for %.2f." % (name, eating_random))
     # client leaves the cafeteria, the place is now vacant
-    # print("%s left at %.2f. the table is now free." % (name, env.now))
+    # print("Customer %s left at %.2f. The table is now free." % (name, env.now))
     yield cafe.table_res_list.put(table)
 
     # fill-in the statistics
-    data = [name, nominal_possibility, interval_after_me, seats_required,
-            arrival_time, pre_booking, queueing_start, queueing_finish,
-            payment_start, payment_finish, booking_start,
-            booking_finish, table, eating_start, eating_finish]
-    df_curr = pd.DataFrame(data=[data],
-                           columns=['Customer', 'NominalPossibility', 'CInterval', 'SeatsRequired',
-                                    'ArrivalTime', 'PreBooking', 'QueueingStart', 'QueueingFinish',
-                                    'PaymentStart', 'PaymentFinish', 'BookingStart',
-                                    'BookingFinish', 'Table', 'EatingStart', 'EatingFinish'])
+    data = [
+        name,
+        nominal_possibility,
+        interval_after_me,
+        seats_required,
+        arrival_time,
+        pre_booking,
+        queueing_start,
+        queueing_finish,
+        payment_start,
+        payment_finish,
+        booking_start,
+        booking_finish,
+        table,
+        eating_start,
+        eating_finish,
+    ]
+    df_curr = pd.DataFrame(
+        data=[data],
+        columns=[
+            "Customer",
+            "NominalPossibility",
+            "CInterval",
+            "SeatsRequired",
+            "ArrivalTime",
+            "PreBooking",
+            "QueueingStart",
+            "QueueingFinish",
+            "PaymentStart",
+            "PaymentFinish",
+            "BookingStart",
+            "BookingFinish",
+            "Table",
+            "EatingStart",
+            "EatingFinish",
+        ],
+    )
     df_stats = pd.concat([df_stats, df_curr], ignore_index=True)
 
 
-def setup(env, food_num, pay_num, c_amount, table_list, probability, seed_val, c_interv):
+def setup(
+    env, food_num, pay_num, c_amount, table_list, probability, seed_val, c_interv
+):
     # creation of a DataFrame for collecting statistics
     global df_stats
-    df_stats = pd.DataFrame(data=[], columns=['Customer', 'NominalPossibility', 'CInterval', 'SeatsRequired',
-                                              'ArrivalTime', 'PreBooking', 'QueueingStart', 'QueueingFinish',
-                                              'PaymentStart', 'PaymentFinish', 'BookingStart',
-                                              'BookingFinish', 'Table', 'EatingStart', 'EatingFinish'])
+    df_stats = pd.DataFrame(
+        data=[],
+        columns=[
+            "Customer",
+            "NominalPossibility",
+            "CInterval",
+            "SeatsRequired",
+            "ArrivalTime",
+            "PreBooking",
+            "QueueingStart",
+            "QueueingFinish",
+            "PaymentStart",
+            "PaymentFinish",
+            "BookingStart",
+            "BookingFinish",
+            "Table",
+            "EatingStart",
+            "EatingFinish",
+        ],
+    )
     # creation of cafeteria
     cafeteria = Cafeteria(env, food_num, pay_num, table_list, seed_val)
 
@@ -170,30 +245,26 @@ def setup(env, food_num, pay_num, c_amount, table_list, probability, seed_val, c
         # arbitrary delay before the next customer
         if c_interv is None:
             # print('No c_interval given')
-            # c_interval_random = abs(stats.norm.rvs(loc=0.3, scale=0.3 / 3, random_state=seed_val + i))
-            c_interval_random = 3
+            c_interval_random = 10
         else:
             # fixed_interval situation
-            c_interval_random = round(c_interv, 2)  # in order to avoid the 0.60000..01 cases
-            # normal distribution (here the c_interv input is a mean)
-            # c_interval_random = abs(stats.norm.rvs(loc=c_interv, scale=c_interv / 3, random_state=seed_val))
-            # exponential distribution (here the c_interv is a scale)
-            # c_interval_random = stats.expon.rvs(loc=0, scale=c_interv, random_state=seed_val)
-            # different interv for one launch: if the number is even:
-            # if i % 2 == 0:
-            #     c_interval_random = round(c_interv, 2)
-            # else:
-            #     c_interval_random = stats.expon.rvs(loc=0, scale=c_interv, random_state=seed_val)
+            c_interval_random = round(
+                c_interv, 2
+            )  # in order to avoid the 0.60000..01 cases
 
-        # version with name: env.process(customer(env, "Customer %d" % i, np.random.randint(1, 5), cafeteria, False))
-        env.process(customer(env, i, np.random.choice(SEATS_LIST, p=[0.3, 0.4, 0.2, 0.1]),
-                             cafeteria, 0, probability, seed_val, c_interval_random))
-        # print("Customer %d appears at %.2f" % (i, env.now))
-
-        # c_interval_random = np.random.exponential(scale=BETA)
-        # c_interval_random = min(0.5, stats.expon.rvs(loc=0, scale=BETA, random_state=seed_val + i))
-        # c_interval_random = stats.erlang.rvs(a=2, loc=0.25, scale=0.1, random_state=seed_val + i)
-        # c_interval_random = abs(stats.norm.rvs(loc=2, scale=0.5, random_state=seed_val + i))
+        env.process(
+            customer(
+                env,
+                i,
+                np.random.choice(SEATS_LIST, p=[0.3, 0.4, 0.2, 0.1]),
+                cafeteria,
+                0,
+                probability,
+                seed_val,
+                c_interval_random,
+            )
+        )
+        # print("Customer %d appears at %.2f" % (i, env.now)
 
         yield env.timeout(c_interval_random)
 
@@ -203,7 +274,18 @@ def launcher(probability, seed_value, client_intervals=None):
     # np.random.seed(seed_value)
     # Create an environment and start the setup process
     env = sp.Environment()
-    env.process(setup(env, FOOD_NUM, PAY_NUM, C_AMOUNT, TABLE_LIST, probability, seed_value, client_intervals))
+    env.process(
+        setup(
+            env,
+            FOOD_NUM,
+            PAY_NUM,
+            C_AMOUNT,
+            TABLE_LIST,
+            probability,
+            seed_value,
+            client_intervals,
+        )
+    )
     # launch
     env.run()
 
@@ -211,3 +293,4 @@ def launcher(probability, seed_value, client_intervals=None):
 
 
 # launcher(1, 3, 0).to_csv('just_to_make_sure.csv', index=False)
+# launcher(0.8, 3)
